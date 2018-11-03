eecbbe9c5f7789d666f5847b215fb6c4142a3004
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-02 21:28:06 -0400

    Set up Stripe redux actions

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
 	renderContent() {
@@ -14,16 +15,18 @@ class Header extends Component {
 					</li>
 				);
 			default:
-				return (
-					<li>
+				return [
+					<li key="add-credit">
+						<Payments />
+					</li>,
+					<li key="logout">
 						<a href="api/logout">Log out</a>
 					</li>
-				);
+				];
 		}
 	}
 
 	render() {
-		console.log(this.props);
 		return (
 			<nav>
 				<div className="nav-wrapper">
