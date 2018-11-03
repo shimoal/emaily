d5e381e7b91b54bedec9ad8a84b4b50e42350a23
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-02 22:13:04 -0400

    Fix typo in billingRoutes

--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -3,7 +3,7 @@ const stripe = require("stripe")(keys.stripeSecretKey);
 
 module.exports = app => {
 	app.post("/api/stripe", async (req, res) => {
-		const charge = await stipe.charges.create({
+		const charge = await stripe.charges.create({
 			amount: 500,
 			currency: "usd",
 			description: "$5 for 5 credits",
