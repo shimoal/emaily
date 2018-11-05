6c73761a57f77bca8e41d7cf9879630025f6d85d
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-05 15:09:01 -0500

    Create routes for production assests

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/billingRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+	// serve production assets in production
+	app.use(express.static("client/build"));
+
+	// serve index.html for all unspecified routes
+	const path = require("path");
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve("__dirname", "client", "build", "index.html"));
+	});
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
