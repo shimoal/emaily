1cf6ff6ff8abb4c928a262bf4f715f35ecda7541
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-05 23:09:39 -0500

    Refactor user

--- services/passport.js
+++ services/passport.js
@@ -9,10 +9,10 @@ passport.serializeUser((user, done) => {
 	done(null, user.id);
 });
 
-passport.deserializeUser((id, done) => {
-	User.findById(id).then(user => {
-		done(null, user);
-	});
+passport.deserializeUser(async (id, done) => {
+	const user = await User.findById(id);
+
+	done(null, user);
 });
 
 passport.use(
