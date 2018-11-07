1837ac6d8f83c6b6f759e1778cf1791b72792c2a
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-07 16:48:43 -0500

    Make survey post route cb async function

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-	app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
+	app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 		const survey = new Survey({
 			title,
@@ -19,6 +19,6 @@ module.exports = app => {
 		});
 
 		const mailer = new Mailer(survey, surveyTemplate(survey));
-		mailer.send();
+		await mailer.send();
 	});
 };
