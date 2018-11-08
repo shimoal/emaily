0462264e2091bc6eb6895038140693894c13f7ba
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-08 13:39:24 -0500

    Save new survey after creation and deduct user credits

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -19,6 +19,15 @@ module.exports = app => {
 		});
 
 		const mailer = new Mailer(survey, surveyTemplate(survey));
-		await mailer.send();
+
+		try {
+			await mailer.send();
+			await survey.save();
+			req.user.credits -= 1;
+			const user = await req.user.save();
+			res.send(user);
+		} catch (err) {
+			res.status(422).send(err);
+		}
 	});
 };
