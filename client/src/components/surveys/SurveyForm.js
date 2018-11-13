70d451af12284113ee987bd519890a8c8d796de0
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-12 21:37:17 -0500

    Style submit button

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -38,7 +38,10 @@ class SurveyForm extends Component {
 			<div>
 				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
 					{this.renderFields()}
-					<button type="submit">Submit</button>
+					<button className="teal btn-flat right white-text" type="submit">
+						Next
+						<i className="material-icons right">done</i>
+					</button>
 				</form>
 			</div>
 		);
