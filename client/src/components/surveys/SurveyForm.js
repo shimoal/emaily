3e90424456f36c6f371b1f884b2bb7e49317c34a
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-12 21:38:36 -0500

    Add cancel button to take user back to dashboard

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
+import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
 
 const FIELDS = [
@@ -38,6 +39,9 @@ class SurveyForm extends Component {
 			<div>
 				<form onSubmit={this.props.handleSubmit(values => console.log(values))}>
 					{this.renderFields()}
+					<Link to="/surveys" className="red btn-flat white-text">
+						Cancel
+					</Link>
 					<button className="teal btn-flat right white-text" type="submit">
 						Next
 						<i className="material-icons right">done</i>
