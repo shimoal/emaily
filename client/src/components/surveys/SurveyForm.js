6567212244ecfef5a4cd716cb8620b6327ae3df0
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-13 12:09:21 -0500

    Validate emails provided

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
 import { Link } from "react-router-dom";
 import SurveyField from "./SurveyField";
+import validateEmails from "../../utils/validateEmails";
 
 const FIELDS = [
 	{
@@ -54,6 +55,8 @@ class SurveyForm extends Component {
 
 function validate(values) {
 	const errors = {};
+
+	errors.emails = validateEmails(values.emails || "");
 
 	FIELDS.forEach(({ name }) => {
 		if (!values[name]) {
