6936b768d905abd650bcaaf93cc5c8e9690fa4fb
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-13 12:44:51 -0500

    Create SurveyFromReview component

--- client/src/components/surveys/SurveyNew.js
+++ client/src/components/surveys/SurveyNew.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import SurveyForm from "./SurveyForm";
+import SurveyFormReview from "./SurveyFormReview";
 
 class SurveyNew extends Component {
+	state = {
+		showFormReview: false
+	};
+
+	renderContent() {
+		return this.state.showFormReview ? <SurveyFormReview /> : <SurveyForm />;
+	}
+
 	render() {
-		return (
-			<div>
-				Survey New
-				<SurveyForm />
-			</div>
-		);
+		return <div>{this.renderContent()}</div>;
 	}
 }
 
