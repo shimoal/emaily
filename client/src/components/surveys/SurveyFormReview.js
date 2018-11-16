86392e045b738f0c348d1b7486b8a36ee785bf62
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-16 10:41:26 -0500

    Create submit button

--- client/src/components/surveys/SurveyFormReview.js
+++ client/src/components/surveys/SurveyFormReview.js
@@ -16,8 +16,15 @@ const SurveyFormReview = ({ onCancel, formValues }) => {
 					);
 				})}
 			</div>
-			<button className="yellow darken-3 btn-flat" onClick={onCancel}>
+			<button
+				className="yellow darken-3 btn-flat white-text"
+				onClick={onCancel}
+			>
 				Back
+			</button>
+			<button className="green btn-flat right white-text">
+				Send Survey
+				<i className="material-icons right">email</i>
 			</button>
 		</div>
 	);
