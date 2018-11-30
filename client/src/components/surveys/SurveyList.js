1a2a525955395d1271c6994972a210b7a65149e4
Alison Reed <alison.michelle.reed@gmail.com>
2018-11-30 17:18:06 -0500

    Add styling for survey cards

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -7,8 +7,32 @@ class SurveyList extends Component {
 		this.props.fetchSurveys();
 	}
 
+	renderSurveys() {
+		console.log("this.props", this.props);
+		return this.props.surveys
+			.map(survey => {
+				return (
+					<div className="card blue-grey darken-1" key={survey._id}>
+						<div className="card-content white-text">
+							<span className="card-title">{survey.title}</span>
+							<p>{survey.body}</p>
+							<p className="right">
+								Sent On: {new Date(survey.dateSent).toLocaleDateString()}
+							</p>
+						</div>
+						<div className="card-action">
+							<a>Yes: {survey.yes}</a>
+
+							<a>No: {survey.no}</a>
+						</div>
+					</div>
+				);
+			})
+			.reverse();
+	}
+
 	render() {
-		return <div>SurveyList</div>;
+		return <div>{this.renderSurveys()}</div>;
 	}
 }
 
